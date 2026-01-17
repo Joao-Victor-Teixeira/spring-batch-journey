38fd278bb74bb5d00529ed5e227d9ca420db4ea6
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-17 14:02:07 -0300

    Project created and Job Config

---
+++ clients-spring-batch/user-request-sb/src/test/java/com/joaodev/user_request_sb/UserRequestSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.user_request_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRequestSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
